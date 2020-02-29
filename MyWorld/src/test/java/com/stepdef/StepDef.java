044253ea03399eba8d37a873b0f09c76e99c68b3
sheiksumon <sheiksumon@localhost>
2020-02-29 01:05:25 -0500

    adding feature

---
+++ MyWorld/src/test/java/com/stepdef/StepDef.java
@@ -0,0 +1,45 @@
+package com.stepdef;
+
+import com.common.Base;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDef extends Base {
+
+	
+	
+	
+
+@Given("^User able to go home page$")
+public void user_able_to_go_home_page() throws Throwable {
+    
+   
+}
+
+@When("^User enter email address$")
+public void user_enter_email_address() throws Throwable {
+    
+   
+}
+
+@When("^user enter password$")
+public void user_enter_password() throws Throwable {
+    
+   
+}
+
+@When("^user click on log in$")
+public void user_click_on_log_in() throws Throwable {
+    
+   
+}
+
+@Then("^User able to sing up successfully$")
+public void user_able_to_sing_up_successfully() throws Throwable {
+    
+   
+}
+
+}
